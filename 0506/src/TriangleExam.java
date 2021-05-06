3113b0ec3ced671ce6b697f85bb67dbb92f45b41
mutation <mutation@naver.com>
2021-05-06 15:00:46 +0900

    0506

---
+++ 0506/src/TriangleExam.java
@@ -0,0 +1,32 @@
+
+public class TriangleExam {
+
+	public static void main(String[] args) {
+		
+		Triangle t = new Triangle(10.2,17.3);
+		System.out.println("변경 전 삼각형 넓이 : "+t.tr());
+		t.cg(7.5,9.2);
+		System.out.println("변경 후 삼각형 넓이 : "+t.tr());
+		
+	}
+
+}
+class Triangle
+{
+	double w;
+	double h;
+	
+	public Triangle(double w,double h) {
+		this.w=w;
+		this.h=h;
+		}
+	public void cg(double w,double h)
+	{
+		this.w=w;
+		this.h=h;
+	}
+	public double tr()
+	{
+		return w*h/2;
+	}
+}
